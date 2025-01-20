69f251c06d9e6c6f64765a9c46c895df76c976f8
yanons <ianonsso2@gmail.com>
2025-01-20 10:20:26 +0300

    подготовка к обработке вопросов

---
+++ attributes/main.php
@@ -0,0 +1,23 @@
+class message {
+    public message;
+    public otvet_one;
+    public otvet_two;
+    public id_message;
+
+    function __construct($otvet, $id_message) {
+        $this->message = $name;
+        return message;
+    }
+    function newmessage($id_message) {
+        $this->message = $name;
+        $this->otvet_one = $otvet_one;
+        $this->otvet_two = $otvet_two;
+        $this->id_message = $id_message;
+        return message;
+    }
+    function reg_init($otvet, $id_message) {
+        $this->message = $name;
+        $this->id_message = $id_message;
+        return message;
+    }
+}
