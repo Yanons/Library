f2916ec9daa65beeaaf3e4d51267c45391525773
yanons <ianonsso2@gmail.com>
2025-01-22 21:44:43 +0300

    db conf

--- app/main.php
+++ app/main.php
@@ -1,4 +1,12 @@
-<?php 
+<?php
+#$STH = $DBH->query('SELECT name, addr, city from folks');  #запуск строки для вывода
+# устанавливаем режим выборки
+#$STH->setFetchMode(PDO::FETCH_ASSOC); 
+# $row = $STH->fetch() #вывод
+# $sql = 'INSERT INTO labels(name) VALUES(:name)'; #строка
+#         $stmt = $this->pdo->prepare($sql); #запуск строки для ввода
+#         $stmt->bindValue(':name', $id_message);
+#         $stmt->execute();
 class message {
     public $message;
     public $otvet_one;
@@ -10,6 +18,14 @@ class message {
         return $message;
     }
     public function newmessage($id_message) {
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email'; #строка
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(['' => '']);
+        $user = $stmt->fetch();
+
+        return $user;
+    }
         $this->message = $name;
         $this->otvet_one = $otvet_one;
         $this->otvet_two = $otvet_two;
